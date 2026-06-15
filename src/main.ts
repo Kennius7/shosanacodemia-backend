925811be22a4d490fca1eb0de00228abf567dda2
Kennius7 <ogbogukenny@yahoo.com>
2026-06-15 12:31:57 +0100

    project init

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio Creator API')
+    .setDescription('API documentation for portfolio creation app')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'http://localhost:5173',
+      'http://localhost:4440',
+      'https://portfolio-pal-app1.netlify.app',
+    ],
+    credentials: true,
+  });
+
+  // Enable NestJS shutdown hooks to gracefully clean up resources (like database connections)
+  app.enableShutdownHooks();
+
+  await app.listen(process.env.PORT ?? 4440);
+}
+void bootstrap();
